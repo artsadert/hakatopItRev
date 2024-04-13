fa76c427636f48c6326b26cff14417d3dfe194ed
k98030118@gmail.com <Khnykin-Artem>
2024-04-13 22:07:53 +0300

    merge 2 pages

--- app/frontend/main.js
+++ app/frontend/main.js
@@ -14,34 +14,20 @@ async function postData(url = "", data = {}) {
   return response.json();
 }
 
-const iphoneNumber = document.getElementById("phoneNumber");
+const phoneNumber = document.getElementById("phoneNumber");
 const email = document.getElementById("email");
-const dataVerification = {
-  number: iphoneNumber.value,
-  email: email.value,
-};
-
-const buttonVerf = document.getElementById("verf");
-buttonVerf.addEventListener("click", async () => {
-  try {
-    await postData("http://localhost/auth", dataVerification);
-    document.cookie = `${iphoneNumber.value}=${email.value}`;
-  } catch (e) {
-    console.log(e);
-  } finally {
-    window.location.pathname = "./action.html";
-  }
-});
-
 const photo = document.getElementById("photo");
 const shopName = document.getElementById("shopName");
-const dataAction = {
-  photo: photo.value,
+
+const data = {
   shopName: shopName.value,
+  number: phoneNumber.value,
+  email: email.value,
+  photo: photo.value
 };
 
 const buttonAction = document.getElementById("action");
 buttonAction.addEventListener(
   "click",
-  async () => await postData("http://localhost/special-shop-price", dataAction)
+  async () => await postData("http://localhost/special-shop-price", data)
 );
