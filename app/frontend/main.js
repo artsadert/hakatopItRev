3b240f9b8972f704e67d64712bbff874bd9149a2
k98030118@gmail.com <Khnykin-Artem>
2024-04-13 18:20:22 +0300

    add cookie

--- app/frontend/main.js
+++ app/frontend/main.js
@@ -14,10 +14,34 @@ async function postData(url = "", data = {}) {
   return response.json();
 }
 
-const iphoneNumber = document.getElementById("iphone-number");
+const iphoneNumber = document.getElementById("phoneNumber");
 const email = document.getElementById("email");
 const dataVerification = {
   number: iphoneNumber.value,
   email: email.value,
 };
-postData('http://localhost/auth', dataVerification);
+
+const buttonVerf = document.getElementById("verf");
+buttonVerf.addEventListener("click", async () => {
+  try {
+    await postData("http://localhost/auth", dataVerification);
+    document.cookie = `${iphoneNumber.value}=${email.value}`;
+  } catch (e) {
+    console.log(e);
+  } finally {
+    window.location.pathname = "./action.html";
+  }
+});
+
+const photo = document.getElementById("photo");
+const shopName = document.getElementById("shopName");
+const dataAction = {
+  photo: photo.value,
+  shopName: shopName.value,
+};
+
+const buttonAction = document.getElementById("action");
+buttonAction.addEventListener(
+  "click",
+  async () => await postData("http://localhost/special-shop-price", dataAction)
+);
