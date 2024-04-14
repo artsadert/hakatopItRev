2987c7dfc66fe979e426768d6f1eb85de4e9d011
gotter <asdkfjh@gmail.com>
2024-04-14 10:53:13 +0300

    final

--- app/backend/submit.py
+++ app/backend/submit.py
@@ -7,23 +7,26 @@ import pandas as pd
 
 from priceChecker.priceCheck import price_check
 
-def submit(imput_folder, output_file):
+def submit(input_folder, output_file):
     data = {"Наименование файла": [], "Наименование товара": [], "Категория продукта": [], "Цена": []}
-    for file_name in listdir(os.path.abspath):
-        res = price_check(abspath("./{folder}/{file_name}.jpg"), abspath("./priceChecker/rostov_prices.json"))
+    dataset_dir = os.path.abspath(f"./{input_folder}")
+    
+    for file_name in listdir(dataset_dir):
+        print(file_name)
+        res = price_check(abspath(f"./{input_folder}/{file_name}"), abspath("./priceChecker/rostov_prices.json"))
         data["Наименование файла"].append(file_name)
         data["Наименование товара"].append(res[1])
-        df = pd.read_excel(abspath("./priceChecker/rostov_prices.json"))
+        df = pd.read_excel(abspath("./priceChecker/prices.xlsx"))
 
-        res = None
+        res_temp = None
         for i in range(len(df)):
             temp_name = df.loc[i, r"Наименование товара/группы товаров"]
             value = df.loc[i, r"Ед. изм."]
             if value is None:
-                res = temp_name
+                res_temp = temp_name
             if res[1] == temp_name:
                 break
-        data["Категория продукта"] = res
+        data["Категория продукта"] = res_temp
         data["Цена"].append(res[4])
     df = DataFrame(data)
     df.to_csv(output_file)
@@ -32,3 +35,4 @@ if __name__ == "__main__":
     folder = input()
     output = input()
     submit(folder, output)
+
